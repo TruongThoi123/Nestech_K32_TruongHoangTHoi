e3c5de8333a27bdba38c7422fc32bd3632944ba7
TruongHoangtThoi <thoi40411@gmail.com>
2024-01-09 20:17:34 +0700

    first commit

---
+++ html/main.js
@@ -0,0 +1,33 @@
+// // // // var fullname = 'Truong Hoang Thoi';
+// // // // var age = '18';
+// // // // console.log(fullname);
+// // // // console.log(age)
+// // // // confirm('xac nhan ban du tuoi');
+// // // // prompt('xac nhan ban du tuoi');
+// // // // setTimeout(function(){
+// // // //     alert('Khai bao');
+// // // // // }, 100)
+// // // setInterval(function(){
+// // //     console.log('Anh Thoi dep trai' + Math.random);
+
+// // // }, 1000)
+
+// // //toan tu so sanh
+// // var a = 1;
+// // var b = 3;
+// // if(a < b && b > a){
+// //     alert('dung');
+// // }
+// var a = 5;
+// var b  = 2;
+// var c = a % b;
+// console.log(c)
+// var languages = [
+//     'javascript',
+//     'PHP',
+//     'Ruby',
+//     'lovely',
+  
+// ];
+// console.log(languages[3])
+let arr =('toyota', 'shibaru', 'FBM')
